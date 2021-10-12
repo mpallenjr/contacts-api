1a632e8c18a44638a643be8826057d9e33938615
“mpallenjr”git config --global user.email “mpallenjnr@gmail.com” <mikeallen@Michaels-MBP.attlocal.net>
2021-10-11 21:39:33 -0500

    recreate contact db, migrate, create three contacts

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,6 +1,2 @@
 class Contact < ApplicationRecord
-  @first_name = first_name
-  @last_name = last_name
-  @email = email
-  @phone_number = phone_number
 end
--- db/migrate/20211012014338_create_contacts.rb
+++ db/migrate/20211012014338_create_contacts.rb
@@ -1,6 +1,10 @@
 class CreateContacts < ActiveRecord::Migration[6.1]
   def change
     create_table :contacts do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.integer :phone_number
 
       t.timestamps
     end
